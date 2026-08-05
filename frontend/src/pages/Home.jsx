c098f1404e66766659df2543571c7800028571bd
mlkhvk <mlkhvkate@gmail.com>
2026-08-05 17:07:21 +0300

    Сделала страницу Главная с использованием MaterialUI

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import { Container, Typography, Button } from "@mui/material";
+export default function Home() {
+    return (
+        <Container>
+            <Typography>Главная</Typography>
+        </Container>
+    );
+}
