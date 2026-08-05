8a54e62a159c90886c51435fa3fd2b4af912322e
mlkhvk <mlkhvkate@gmail.com>
2026-08-06 00:59:53 +0300

    Сделала модальное окно

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import { Container, Typography } from '@mui/material';
+import {Button, Container, Typography} from '@mui/material';
+import {useState} from 'react';
+import CustomModal from '../components/CustomModal'
 export default function Home() {
+    const[open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true)
+    };
+    const handleClose = () => {
+        setOpen(false)
+    };
     return (
-        <Container>
+        <Container className='min-h-screen py-8 flex flex-col items-center gap-6'>
             <Typography>Главная</Typography>
+            <Button variant='contained' onClick={handleOpen}>Клик</Button>
+            <CustomModal open={open} onClose={handleClose}/>
         </Container>
     );
 }
