accf58a64fda50d5c594e9531b571c5901b488e7
mlkhvk <mlkhvkate@gmail.com>
2026-08-07 13:41:22 +0300

    пытаемся подключить бек

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000/',
-        changeOrigin: true,
-        secure: false,
-      }
+    plugins: [react(), tailwindcss()],
+      server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000/',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
     }
-  }
-})
+});
