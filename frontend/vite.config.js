e8e71295561eabc2a7c4a64a81a94c9d982aebb2
mlkhvk <mlkhvkate@gmail.com>
2026-08-07 13:16:25 +0300

    убрала clsx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-});
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000/',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
