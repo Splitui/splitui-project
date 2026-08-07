172729afa8a6454d2d1d6e05c4a1d1acbedcf0ef
IceKeePeRR <vovaokulovski2013@ya.ru>
2026-08-07 14:52:57 +0300

    исправил недочёты в meeting

--- frontend/src/pages/Meeting.jsx
+++ frontend/src/pages/Meeting.jsx
@@ -5,7 +5,7 @@ import MembersTab from '../components/meetingTabs/MembersTab';
 import ReceiptsTab from '../components/meetingTabs/ReceiptsTab';
 
 export default function Meeting() {
-    const [value, setValue] = useState('one');
+    const [value, setValue] = useState('members');
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -20,12 +20,13 @@ export default function Meeting() {
     return (
         <div className="min-h-screen pb-20">
             <main>
-                {value == 'one' && <MembersTab />} {value == 'two' && <ReceiptsTab />}
+                {value === 'members' && <MembersTab />}{' '}
+                {value === 'receipts' && <ReceiptsTab />}
             </main>
             <nav className="fixed bottom-0 left-0 w-full bg-white border-t-2 border-black z-50">
                 <Tabs value={value} onChange={handleChange} variant="fullWidth">
-                    <Tab value="one" label="Участники" className="font-bold py-5" />
-                    <Tab value="two" label="Чеки" className="font-bold py-5" />
+                    <Tab value="members" label="Участники" className="font-bold py-5" />
+                    <Tab value="receipts" label="Чеки" className="font-bold py-5" />
                 </Tabs>
             </nav>
         </div>
